28c70661be9fbf54b0f7ab768f5b76c5d56d3995
SHZTD <tdrag@elpuig.xeill.net>
2025-04-03 19:32:27 +0200

    subir datos a ES, establecer conexion a la NoSQL, y mejora de vista de logs.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         VerLogs vl = new VerLogs();
-        vl.hacerQuery(
-                "FROM logs_server " +
-                        "| KEEP ip, timestamp, url " +
-                        "| LIMIT 5"
-        );
+        // selecciona los 10 primeros campos
+        vl.hacerQuery("FROM logs_server | LIMIT 10");
+
+        // obtener solo las peticiones con errores (status >= 400) y las primeras 20
+        vl.hacerQuery("FROM logs_server | WHERE status >= 400 | KEEP ip, timestamp, method, url, status | LIMIT 20");
+
+        // selecciona las ips que han hecho mas requests
+        vl.hacerQuery("FROM logs_server | STATS request_count = COUNT(*) BY ip | SORT request_count DESC | LIMIT 1");
     }
 }
--- src/main/java/org/example/VerLogs.java
+++ src/main/java/org/example/VerLogs.java
@@ -5,6 +5,8 @@ import org.apache.http.util.EntityUtils;
 import org.elasticsearch.client.Request;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -13,22 +15,24 @@ public class VerLogs {
 
     public void hacerQuery(String esqlQuery) {
 
-        // construye bien la query
-        String actualQuery =
-                "{\n" +
-                "   \"query\": \"" + esqlQuery + "\"\n" +
-                "}";
-
-        // System.out.println(actualQuery); // DEBUG
+        String actualQuery = "{ \"query\": \"" + esqlQuery + "\" }";
 
         try (RestClient client = RestClient.builder(new HttpHost("localhost", 9200, "http")).build()) {
             Request request = new Request("POST", "/_query");
             request.setJsonEntity(actualQuery);
-
             Response response = client.performRequest(request);
-
             String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
-            System.out.println("Resultados de la consulta ES|QL:\nQuery hecha: " + esqlQuery + "\n" + responseBody);
+
+            // jackson para mapear bien el objeto
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonResponse = objectMapper.readTree(responseBody);
+
+            // extraemos solo la parte de "values"
+            JsonNode valuesNode = jsonResponse.get("values");
+
+            // formatamos
+            System.out.println("Resultados de la consulta ES|QL:\nQuery hecha: " + esqlQuery);
+            System.out.println(valuesNode.toPrettyString() + "\n");
 
         } catch (IOException e) {
             e.printStackTrace();
